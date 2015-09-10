258558fd71d13e486b53a5152d628adc72aa97c8
Geoff Cranmer <cranmerg@Geoffs-MacBook-Pro.local>
2015-09-10 12:25:05 -0400

    Added pattern link script

---
+++ scripts/pln.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pattern Link
+# This script creates symbolic links using a pattern specified as an argument.
+
+# Unofficial Bash Strict Mode
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+SOURCE=$1
+TARGET=$2
+
+for FILE in $SOURCE*; do
+  ln -s "$FILE" $(echo "$FILE" | sed "s/$SOURCE/$TARGET/");
+done
+
