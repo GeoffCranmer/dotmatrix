5e9626be2c13b110441f4f90c156cd338f5dea24
Geoff Cranmer <cranmerg@Geoffs-MacBook-Pro.local>
2015-09-10 12:26:01 -0400

    Added 24-hour copy script

---
+++ scripts/24hcp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script copies all contents of a directory that have been modified within 
+# 24 hours
+
+# Unofficial Bash Strict Mode
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+SOURCE=$1
+TARGET=$2
+
+find $SOURCE -mtime 0 -exec cp --parents {} $TARGET \;
+
