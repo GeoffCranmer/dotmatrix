beaabb4299dc160c7f2d37aead6eb867a790df88
Geoff Cranmer <cranmerg@Geoffs-MacBook-Pro.local>
2015-10-13 11:09:44 -0400

    Add RoseRT SVN properties script

---
+++ scripts/rrtsvnprops.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Rose RealTime SVN Properties
+# Set svn:needslock on all non-mergeable RoseRT file types
+
+# Unofficial Bash Strict Mode
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+find . -name *.rtclass -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtcmppkg -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtdeploy -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtlogpkg -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtcmp -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtcmpdgm -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtclassdgm -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtcollab -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtdeploydgm -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtintractn -exec svn propset svn:needs-lock yes {} \;
+find . -name *.rtprcsr -exec svn propset svn:needs-lock yes {} \;
