11b618c41508f39e25c15902016061a55b91efb6
muffinkilla <geoff.cranmer@gmail.com>
2015-12-31 12:00:25 -0500

    Add script to check for open files

---
+++ scripts/check_for_open_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check for Open Files
+# Device or resource busy? Find the culprit.
+
+# Unofficial Bash Strict Mode
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+PATH=$1
+
+lsof +D $PATH
